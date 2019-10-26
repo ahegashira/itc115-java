434f63118622d750c1b18f1a9073180ec51ed168
Hiro Egashira <ahegashira@gmail.com>
2019-10-26 10:29:03 -0700

    Create assignment5.java

---
+++ assignment5.java
@@ -0,0 +1,19 @@
+public static String season(int month, int day) {
+    String currentSeason = "";
+    if ((month == 12 && day > 15) || (month == 1 || month == 2) || (month == 3 && day < 16)) {
+       currentSeason = "Winter"; 
+    } 
+    else if 
+       ((month == 3 && day > 15) || (month == 4 || month == 5) || (month == 6 && day < 16)) {
+       currentSeason = "Spring";
+    } 
+    else if 
+       ((month == 6 && day > 15) || (month == 7 || month == 8) || (month == 9 && day < 16)) {
+       currentSeason = "Summer";
+    } 
+    else if
+       ((month == 9 && day > 15) || (month == 10 || month == 11) || (month == 12 && day < 16)) {
+       currentSeason = "Fall";
+    }   
+    return currentSeason;
+}
