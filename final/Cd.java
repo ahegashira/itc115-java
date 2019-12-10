d8732af1eb9743658bfcfb6f5621a0f353d811f0
Hiro Egashira <ahegashira@gmail.com>
2019-12-09 18:46:21 -0800

    Update Cd.java

--- final/Cd.java
+++ final/Cd.java
@@ -1,12 +1,14 @@
 package library;
 
 import java.text.NumberFormat;
+import java.time.LocalDate;
 
 public class Cd implements Item {
 	public String title;
 	public String authName;
 	public String publisher;
 	public int isbn;
+	public LocalDate dueDate = LocalDate.now();
 	public double lateFine = 0.50;
 
 	public Cd(String title1, String authName1, String publisher1, int isbn1) {
@@ -25,7 +27,8 @@ public class Cd implements Item {
 		return "We hope you enjoyed " + title +
 			"\nBy: " + authName +
 			"\nPublisher: " + publisher +
-			"\nISBN: " + isbn + 
+			"\nISBN: " + isbn +
+			"\nDue 10 days from: " + dueDate +
 			"\nIf this item is returned late, the fee is " + getPriceFormatted(lateFine) + " per day." + 
 			"\n\n\n";
 	}
@@ -92,5 +95,29 @@ public class Cd implements Item {
 	public int isbn() {
 		// TODO Auto-generated method stub
 		return 0;
+	}
+
+	@Override
+	public boolean reserved() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean checkedIn() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double finesAccrued() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public LocalDate dueDate() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
