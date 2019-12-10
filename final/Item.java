1aea786feb6b70b0eb2c87337663f594a2062f9b
Hiro Egashira <ahegashira@gmail.com>
2019-12-09 18:47:00 -0800

    Update Item.java

--- final/Item.java
+++ final/Item.java
@@ -1,8 +1,14 @@
 package library;
 
+import java.time.LocalDate;
+
 public interface Item {
 	public String title();
 	public String authName();
 	public String publisher();
 	public int isbn(); 
+	public LocalDate dueDate();
+	public boolean reserved();
+	public boolean checkedIn();
+	public double finesAccrued();
 }
